f10024678a95885f97e80fa68ea6ad62259367f9
Hrant Grishyan <hrant_grishyan@epam.com>
2024-06-02 12:20:50 +0400

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
 
-function App() {
+const tg = window.Telegram.WebApp;
+
+const App = () => {
+
+    useEffect(() => {
+        tg.ready();
+    }, []);
+
+    const handleClose = () => {
+        tg.close();
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        hello
+      <button onClick={handleClose}>Close</button>
     </div>
   );
 }
