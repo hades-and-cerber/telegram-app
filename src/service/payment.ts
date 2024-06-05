7c234dd90a14e4f1a2bbd9a06a2725eec204c26a
Hrant Grishyan <hrant_grishyan@epam.com>
2024-06-05 16:36:14 +0400

    feat: comment unused code

--- src/service/payment.ts
+++ src/service/payment.ts
@@ -1,8 +1,9 @@
-import axios, { AxiosResponse } from 'axios';
-import { DepositAddress } from '../types/dto';
+// import axios, { AxiosResponse } from 'axios';
+// import { DepositAddress } from '../types/dto';
+//
+// const BASE_URL = 'http://localhost:3000/api/';
 
-const BASE_URL = 'http://localhost:3000/api/';
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const getDepositAddress = async (token: string, chain: string) => {
   // const response: AxiosResponse<DepositAddress> = await axios.post(`${BASE_URL}deposit`, {
   //   userId: 1,
